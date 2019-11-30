1f9d0acf63dabcc47115a74feb1d900d5ad51011
nicksettler <moiseevnikita14@gmail.com>
2019-11-30 13:39:16 +0500

    #hotfix001 - changed entry & deleted source

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = env => {
     const isProd = process.env.production;
 
     return {
-        entry: path.resolve(__dirname, "src/index.ts"),
+        entry: path.resolve(__dirname, "src/index.js"),
         context: path.resolve(__dirname, "src"),
         mode: isProd ? "production" : "development",
         devtool: "inline-source-map",
