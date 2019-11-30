2b2478ad8e84a2010eb7781ff1da619f64a86a46
nicksettler <moiseevnikita14@gmail.com>
2019-11-30 19:09:08 +0500

    build added

---
+++ black-friday/build/precache-manifest.cd4645e0ccdcb22f11e97db1a003a7f8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d562e57cc38d4b0acf66b5d868383c1d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e1bfd73b059467c8039c",
+    "url": "/static/css/main.d6bd68b4.chunk.css"
+  },
+  {
+    "revision": "34db522cde5f700cb6ff",
+    "url": "/static/js/2.bde6ae0b.chunk.js"
+  },
+  {
+    "revision": "e1bfd73b059467c8039c",
+    "url": "/static/js/main.e5cb4c36.chunk.js"
+  },
+  {
+    "revision": "f9ce723117725415d948",
+    "url": "/static/js/runtime-main.8ea8dfba.js"
+  }
+]);
